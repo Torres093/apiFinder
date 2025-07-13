4d0360e8d9a8d3ed1fe85b14d443f318577273e4
Torres <127458733+Torres093@users.noreply.github.com>
2025-07-12 21:18:07 -0600

    small

--- src/main/java/grupoExpo/API/Models/DTO/UserDTO.java
+++ src/main/java/grupoExpo/API/Models/DTO/UserDTO.java
@@ -1,4 +1,19 @@
 package grupoExpo.API.Models.DTO;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class UserDTO {
+
+    private Long idusuario;
+
+
+
+
 }
